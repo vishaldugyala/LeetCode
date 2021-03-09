c04776ff3252af5c2953ac2f3180f9ded5797627
vishaldugyala <vishaldugyala@gmail.com>
2021-03-09 20:17:02 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.43%) - LeetHub

---
+++ power-of-two/power-of-two.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        while(n>=2)
+        {
+            if(n%2!=0)
+                return false;
+            n /= 2;
+        }
+        
+        return (n==1);
+    }
+};
