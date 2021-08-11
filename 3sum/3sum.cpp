aa7da39723600d4eed0ae24da423c9fff365ea19
vishaldugyala <vishaldugyala@gmail.com>
2021-08-11 08:48:18 +0530

    Time: 68 ms (75.53%), Space: 21.2 MB (41.65%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -2,42 +2,50 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         
-        set<vector<int>> sol;
+        vector<vector<int>> sol;
         
+        int len = nums.size();
         sort(nums.begin(),nums.end());
         
-        for(int i=0;i<nums.size();i=i+1)
+        for(int i=0;i<len;i=i+1)
         {
             int j=i+1;
-            int k=nums.size()-1;
+            int k=len-1;
             int T = 0-nums[i];
           //  cout<<i<<" "<<j<" "<<k<<" "<<T<<endl;
             while(j<k)
             {
+              //  cout<<i<<" "<<j<<" "<<k<<" "<<sol.size()<<endl;
                 if(nums[j]+nums[k] < T)
                     j=j+1;
                 else if(nums[j]+nums[k]>T)
                     k=k-1;
                 else
                 {
-                   // cout<<"YES"<<endl;
+                  //  cout<<"YES"<<endl;
                     vector<int> temp{nums[i],nums[j],nums[k]};
-                    sol.insert(temp);
-                    j=j+1;
-                    k=k-1;
+                    sol.push_back(temp);
+                  //  if(j<len-1 && nums[j] == nums[j+1])
+                   // {
+                        while(j<len-1 && nums[j] == nums[j+1])
+                        j += 1;
+                   // }
+                   // else
+                        j += 1;
+                    
+                   // if(k>0 && nums[k-1]==nums[k])
+                   // {
+                        while(k>0 && nums[k-1]==nums[k])
+                        k -= 1;
+                   // }
+                   // else
+                        k -= 1;
                 }
+                
             }
+            while(i<len-1 && nums[i] == nums[i+1])
+                i += 1;
         }
-        vector<vector<int>> sol_return;
-        
-        set<vector<int>>::iterator it;
-        
-        for(it=sol.begin();it!=sol.end();it++)
-        {
-            vector<int> temp = *it;
-            sol_return.push_back(temp);
-        }
-        
-        return sol_return;
+      return sol;
     }
 };
