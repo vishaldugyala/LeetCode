8c269976ce5212ab22bce7312b3de9d8d61d5af9
vishaldugyala <vishaldugyala@gmail.com>
2021-08-27 19:03:15 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (41.57%) - LeetHub

--- house-robber/house-robber.cpp
+++ house-robber/house-robber.cpp
@@ -4,22 +4,23 @@ public:
         
         int N = nums.size();
         
-        if(N == 1)
+ /*       if(N == 1)
             return nums[0];
         else if(N == 2)
             return max(nums[0],nums[1]);
         else if(N == 3)
             return max(nums[0]+nums[2],nums[1]);    
-        
+            
+ */       
         vector<int> dp(nums.size(),0);
         
         for(int i=nums.size()-1;i>-1;i=i-1)
         {
-            if(i == nums.size()-1)
+            if(i == N-1)
                 dp[i] = nums[i];
-            else if(i == nums.size()-2)
+            else if(i == N-2)
                 dp[i] = max(nums[i],nums[i+1]);
-            else if(i == nums.size()-3)
+            else if(i == N-3)
                 dp[i] = max(nums[i]+nums[i+2],nums[i+1]);
             else
                 dp[i] = max(nums[i] + dp[i+2],nums[i+1] + dp[i+3]);
