c3db65c275a18e0f9ba33a0aac500282377ff09d
vishaldugyala <vishaldugyala@gmail.com>
2021-08-29 20:47:35 +0530

    Time: 36 ms (61.97%), Space: 27.5 MB (56.49%) - LeetHub

---
+++ binary-search/binary-search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int lo = 0;
+        int hi = nums.size()-1;
+        
+        while(lo<=hi)
+        {
+            int mid = lo+(hi-lo)/2;
+            
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] > target)
+                hi = mid-1;
+            else if(nums[mid] < target)
+                lo = mid+1;
+        }
+        
+        return -1;
+    }
+};
